e667ad0b2ebfd58b93670b781ce39290f4ce61fb
Joan Manrubia Martinez <joan.manrubia@enti.cat>
2019-10-23 11:48:07 +0200

    Added navmesh and enemy behaviour

---
+++ GameJam_2019-2020/Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI; 
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Enemy")]
+    private int health;
+    private int damage; 
+    private Vector3 position;
+    public NavMeshAgent agent;
+
+    [Header("Player")]
+    public GameObject player;
+    private Vector3 posPlayer;
+
+    private void Update() {
+        posPlayer = player.transform.position;
+        agent.SetDestination(posPlayer); 
+    }
+}
