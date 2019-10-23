3fdd5a5641d7fcb1e681f730a56e1c61f2e4d1e6
Pau <pau.sole@enti.cat>
2019-10-23 12:43:45 +0200

    UI_v1.0
    Simple healthabr and score

---
+++ GameJam_2019-2020/Assets/Scripts/UI.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    public Image bgSlider;
+    public Image playerHealth;
+    public Slider slider;
+
+    public Text scoreText;
+
+    public int score;
+    public int PlayerStartingHealth;
+    public int currentPlayerHealth;
+    public int DamageDealt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "Score: ";
+        SetupSlider(PlayerStartingHealth);
+        currentPlayerHealth = PlayerStartingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetScore(score);
+        UpdateSlider(currentPlayerHealth);
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            RecieveDamage(DamageDealt);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            EnemyKilled(20);
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pauseMenu.activeInHierarchy)
+                PauseGame();
+            else if (pauseMenu.activeInHierarchy)
+                ContinueGame();
+        }
+    }
+
+    public void SetScore(int enemyScore)
+    {
+        scoreText.text = "Score: " + enemyScore;
+    }
+
+    public void SetupSlider(int playerHealth)
+    {
+        slider.minValue = 0;
+        slider.maxValue = playerHealth;
+        slider.value = playerHealth;
+    }
+
+    public void UpdateSlider(int currentHealth)
+    {
+        slider.value = currentHealth;
+        if (currentHealth >= 50)
+            playerHealth.color = Color.green;
+        if (currentHealth < 50)
+            playerHealth.color = Color.yellow;
+        if (currentHealth < 25)
+            playerHealth.color = Color.red;
+    }
+
+    public int RecieveDamage(int damage)
+    {
+        currentPlayerHealth -= damage;
+        return currentPlayerHealth;
+    }
+
+    public void EnemyKilled(int enemyPoints)
+    {
+        score += enemyPoints;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        pauseMenu.SetActive(true);
+        
+    }
+
+    public void ContinueGame()
+    {
+        Time.timeScale = 1;
+        pauseMenu.SetActive(false);
+    }
+
+}
