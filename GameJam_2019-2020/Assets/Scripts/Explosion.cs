d87a86c23f611c90b4a63bb82cf7672490441a2b
jmanrumartinez <jmanrumartinez@gmail.com>
2019-10-24 12:35:13 +0200

    Enemy spawner system and enemy

---
+++ GameJam_2019-2020/Assets/Scripts/Explosion.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    private float time = 0;
+
+    private void Update() {
+        time += Time.deltaTime;
+
+        if (time >= 0.5f) {
+            Destroy(this.gameObject); 
+        }
+    }
+
+}
